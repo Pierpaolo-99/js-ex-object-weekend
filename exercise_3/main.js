bdcee2b9661445c1cbfa052ccdc0a960d3166cc5
Pierpaolo-99 <pierpaolodellioio@gmail.com>
2025-10-01 15:30:10 +0200

    Aggiungi struttura HTML e funzioni per il filtraggio e l'estrazione di valori

---
+++ exercise_3/main.js
@@ -0,0 +1,12 @@
+/* traccia : Scrivi una funzione conteggioParole(str) che restituisca un oggetto in cui le chiavi sono le parole e i valori il numero di volte che compaiono nella stringa.
+
+console.log(conteggioParole("ciao mondo ciao a tutti mondo")); 
+// Output: { ciao: 2, mondo: 2, a: 1, tutti: 1 }*/
+
+function conteggioParole(str){
+    const objectWord = {}
+    let word;
+    for (let i = 0; i < str.length; i++){
+
+    }
+}
